071f0a2db20bfda8ae2ca9ac7d3e2eba8b4e693e
thanhnt <buitienthanhnt@gmail.com>
2026-04-05 23:49:03 +0700

    dev: Update code for data-list

--- resources/js/pages/akhoglobal/Manage.tsx
+++ resources/js/pages/akhoglobal/Manage.tsx
@@ -45,6 +45,39 @@ const Manage: FunctionComponent<InertiaConfig['sharedPageProps']> = () => {
           </DropdownMenuContent>
         </DropdownMenuPortal>
       </DropdownMenu>
+
+      {/* Trong HTML, để tạo danh sách gợi ý cho một trường nhập liệu (form field), bạn sử dụng thẻ <datalist> kết hợp với thuộc tính list của thẻ <input>. 
+        Cách thức hoạt động:
+
+        Thẻ <datalist>: Chứa các thẻ <option> định nghĩa những giá trị gợi ý.
+
+        Liên kết: Bạn gán một id duy nhất cho <datalist> và tham chiếu id đó trong thuộc tính list của thẻ <input>.
+
+        Trải nghiệm người dùng: Khi người dùng nhập liệu, trình duyệt sẽ hiển thị một menu thả xuống chứa các tùy chọn khớp với nội dung đang nhập (tính năng tự động hoàn
+
+        Các điểm cần lưu ý:
+
+        Tính linh hoạt: Khác với thẻ <select> (bắt buộc chọn trong danh sách), <datalist> vẫn cho phép người dùng tự nhập các giá trị không có sẵn.
+
+        Hỗ trợ loại Input: Ngoài loại text, nó còn hoạt động tốt với các loại như date, time, number, range, và color.
+
+        Giao diện: Trình duyệt tự quyết định cách hiển thị danh sách này; bạn rất khó dùng CSS để tùy chỉnh kiểu dáng trực tiếp của menu thả xuống.
+
+        Dùng cho nhiều trường: Bạn có thể dùng một <datalist> duy nhất cho nhiều thẻ <input> khác nhau bằng cách gán cùng một id vào thuộc tính list của chúng. 
+      */}
+      <div className='bg-blue-100 my-4'>
+        <label htmlFor="browser-input">Chọn trình duyệt bạn dùng:</label>
+        <input list="browsers" id="browser-input" name="browser" placeholder="Gõ để tìm kiếm..." />
+
+        <datalist id="browsers">
+          <option value="Chrome" />
+          <option value="Firefox" />
+          <option value="Safari" />
+          <option value="Edge" />
+          <option value="Opera" />
+        </datalist>
+      </div>
+
     </div>
   )
 }
