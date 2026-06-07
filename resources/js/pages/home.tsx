875bb4160677e968874b92a71f306ea44a977ea3
thanhnt <buitienthanhnt@gmail.com>
2026-06-07 21:32:18 +0700

    dev-akho: Update code

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,7 +1,5 @@
 
 import type { InertiaConfig } from '@inertiajs/core';
-import { Link } from '@inertiajs/react';
-import { manage } from '@/routes/akho';
 
 export default function Home(props: InertiaConfig['sharedPageProps']) {
 
@@ -10,7 +8,7 @@ export default function Home(props: InertiaConfig['sharedPageProps']) {
       <h2>demo home page</h2>
       <h3>{props.demo as string}</h3>
       {/* <Link href={manage.url({id: 12})}>Manage</Link> */}
-      <Link href={manage.url()}>Manage</Link>
+
     </div>
   );
 }
